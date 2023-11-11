fa96deb06aa473f65bc0b9bd75453328c6c41b6b
Francis McCarthy <francis.john.mccarthy@googlemail.com>
2023-11-11 18:48:40 +0100

    ch10

---
+++ ch10 - Debugging/ex02.sh
@@ -0,0 +1,7 @@
+# Modify the previous exercise so that script continues, even if an error occurs. This time all three ls commands will execute.
+
+#!/bin/bash -x
+
+ls /etc/passwd
+ls /move/along/nothing/to/see/here
+ls /etc/passwd
